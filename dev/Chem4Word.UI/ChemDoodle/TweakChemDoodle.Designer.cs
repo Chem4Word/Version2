b03baf158f52552dd080dc0fdde9471b89f36408
Mike Williams <mikewill34@yahoo.com>
2013-11-08 23:00:49 +0000

    First fork for ChemDoodle

---
+++ dev/Chem4Word.UI/ChemDoodle/TweakChemDoodle.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Chem4Word.UI.ChemDoodle
+{
+    partial class TweakChemDoodle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(408, 330);
+            this.browser.TabIndex = 0;
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.browser_DocumentCompleted);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(320, 336);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.Location = new System.Drawing.Point(239, 336);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // TweakChemDoodle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 371);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.browser);
+            this.Name = "TweakChemDoodle";
+            this.Text = "TweakChemDoodle";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TweakChemDoodle_FormClosing);
+            this.Load += new System.EventHandler(this.TweakChemDoodle_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
---
+++ dev/Chem4Word.UI/Converters/Json.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Chem4Word.UI.Converters
+{
+    public static class Json
+    {
+        public static string ToCML(string p_JsonIn)
+        {
+            Debug.WriteLine("JSON.ToCML()");
+            string result = p_JsonIn;
+
+            // http://www.xml-cml.org/convention/molecular
+            if (p_JsonIn.StartsWith("{"))
+            {
+                XmlDocument doc = new XmlDocument();
+
+                int atomCounter = 0;
+                int bondCounter = 0;
+
+                #region Construct empty CML file
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<cml:cml convention='conventions:molecular'");
+                sb.Append(" xmlns:p7='http://www.xml-cml.org/dictionary/cmlx/'");
+                sb.Append(" xmlns:conventions='http://www.xml-cml.org/convention/'");
+                sb.Append(" xmlns:cml='http://www.xml-cml.org/schema'");
+                sb.Append(" xmlns:cmlDict='http://www.xml-cml.org/dictionary/cml/'");
+                sb.Append(" xmlns:nameDict='http://www.xml-cml.org/dictionary/cml/name/'");
+                sb.Append(">");
+                sb.Append("<cml:molecule id='m1'>");
+                sb.Append("  <cml:atomArray>");
+                sb.Append("  </cml:atomArray>");
+                sb.Append("  <cml:bondArray>");
+                sb.Append("  </cml:bondArray>");
+                sb.Append("</cml:molecule>");
+                sb.Append("</cml:cml>");
+                doc.LoadXml(sb.ToString());
+                #endregion
+
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+                nsmgr.AddNamespace("cml", "http://www.xml-cml.org/schema");
+
+                XmlElement cmlAtoms = doc.SelectSingleNode("//cml:atomArray", nsmgr) as XmlElement;
+                XmlElement cmlBonds = doc.SelectSingleNode("//cml:bondArray", nsmgr) as XmlElement;
+
+                JToken molJson = JObject.Parse(p_JsonIn);
+
+                JToken atoms = molJson.SelectToken("a");
+                Debug.WriteLine("Found " + atoms.Count<JToken>() + " atoms");
+                foreach (JToken atom in atoms)
+                {
+                    // Ignore incoming atom id
+                    //string atomid = (string)atom.SelectToken("i");
+                    string label = (string)atom.SelectToken("l");
+                    double x = (double)atom.SelectToken("x");
+                    double y = (double)atom.SelectToken("y");
+                    string charge = (string)atom.SelectToken("c");
+
+                    XmlElement cmlAtom = doc.CreateElement("cml:atom", "http://www.xml-cml.org/schema");
+                    cmlAtom.SetAttribute("id", "a" + atomCounter.ToString());
+                    if (label == null)
+                    {
+                        label = "C";
+                    }
+                    cmlAtom.SetAttribute("elementType", label);
+                    cmlAtom.SetAttribute("x2", x.ToString());
+                    cmlAtom.SetAttribute("y2", y.ToString());
+                    if (charge != null)
+                    {
+                        cmlAtom.SetAttribute("formalCharge", charge);
+                    }
+                    cmlAtoms.AppendChild(cmlAtom);
+                    atomCounter++;
+                }
+
+                JToken bonds = molJson.SelectToken("b");
+                if (bonds != null)
+                {
+                    Debug.WriteLine("Found " + bonds.Count<JToken>() + " bonds");
+                    foreach (JToken bond in bonds)
+                    {
+                        // Ignore incoming bond id
+                        //string bondid = (string)bond.SelectToken("i");
+                        string begin = (string)bond.SelectToken("b");
+                        string end = (string)bond.SelectToken("e");
+                        string order = (string)bond.SelectToken("o");
+                        string stereo = (string)bond.SelectToken("s");
+
+                        XmlElement cmlBond = doc.CreateElement("cml:bond", "http://www.xml-cml.org/schema");
+                        cmlBond.SetAttribute("id", "b" + bondCounter);
+                        cmlBond.SetAttribute("atomRefs2", "a" + begin + " a" + end);
+                        cmlBond.SetAttribute("cyclic", "Acyclic");
+                        cmlBond.SetAttribute("p7", "http://www.xml-cml.org/dictionary/cmlx");
+
+                        if (order != null)
+                        {
+                            cmlBond.SetAttribute("order", OrderToString(order));
+                        }
+                        else
+                        {
+                            cmlBond.SetAttribute("order", "S");
+                        }
+
+                        if (stereo != null)
+                        {
+                            XmlElement cmlStereo = doc.CreateElement("cml:bondStereo", "http://www.xml-cml.org/schema");
+                            cmlStereo.InnerText = StereoToString(stereo);
+                            cmlBond.AppendChild(cmlStereo);
+                        }
+                        cmlBonds.AppendChild(cmlBond);
+                        bondCounter++;
+                    }
+                }
+                result = Beautify(doc);
+            }
+            return result;
+        }
+
+        private static string StereoToString(string p_stereo)
+        {
+            string result = "";
+            switch (p_stereo)
+            {
+                case "protruding":
+                    result = "W";
+                    break;
+                case "recessed":
+                    result = "H";
+                    break;
+                case "ambiguous":
+                    result = "S";
+                    break;
+            }
+            return result;
+        }
+
+        private static string OrderToString(string p_order)
+        {
+            string result = "p_order";
+            switch (p_order)
+            {
+                case "1":
+                case "1.0":
+                    result = "S";
+                    break;
+                case "2":
+                case "2.0":
+                    result = "D";
+                    break;
+                case "3":
+                case "3.0":
+                    result = "T";
+                    break;
+            }
+            return result;
+        }
+
+        private static string Beautify(XmlDocument doc)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = "  ";
+            settings.NewLineChars = "\r\n";
+            settings.NewLineHandling = NewLineHandling.Replace;
+            using (XmlWriter writer = XmlWriter.Create(sb, settings))
+            {
+                doc.Save(writer);
+            }
+            return sb.ToString();
+        }
+
+    }
+}
