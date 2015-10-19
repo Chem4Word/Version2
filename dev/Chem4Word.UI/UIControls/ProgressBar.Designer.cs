498a865df7cd47579e4aeb97771dbb468918676f
Mike Williams <MikeWilliams-UK@users.noreply.github.com>
2015-10-19 13:57:14 +0100

    Added pop up form with progress bar while rendering OOXML molecule

---
+++ dev/Chem4Word.UI/UIControls/ProgressBar.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Chem4Word.UI.UIControls
+{
+    partial class ProgressBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(570, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(570, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // ProgressBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 78);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ProgressBar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Chem4Word ProgressBar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ dev/Chem4Word.UI/UIControls/ProgressBar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chem4Word.UI.UIControls
+{
+    public partial class ProgressBar : Form
+    {
+        public int Value
+        {
+            get { return progressBar1.Value; }
+            set { progressBar1.Value = value; }
+        }
+
+        public int Minimum
+        {
+            get { return progressBar1.Minimum; }
+            set { progressBar1.Minimum = value; }
+        }
+
+        public int Maximum
+        {
+            get { return progressBar1.Maximum; }
+            set { progressBar1.Maximum = value; }
+        }
+
+        public string Message
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+        public ProgressBar()
+        {
+            InitializeComponent();
+        }
+
+        public void Increment(int value)
+        {
+            progressBar1.Increment(value);
+            textBox1.SelectionStart = 0;
+            textBox1.SelectionLength = 0;
+            progressBar1.Refresh();
+            Application.DoEvents();
+        }
+    }
+}
