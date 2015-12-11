227e8342ac20696b12f847bbe07fa7e70bf77d91
Mike Williams <MikeWilliams-UK@users.noreply.github.com>
2015-12-11 15:45:21 +0000

    Tweak Office Guid detection

--- dev/Chem4Word.Common/Telemetry.cs
+++ dev/Chem4Word.Common/Telemetry.cs
@@ -98,7 +98,7 @@ namespace Chem4Word.Common
 #if DEBUG
                 me4.Message = _helper.AddInVersion + " (debug)";
 #else
-                me4.Message = _helper.AddInVersion + " (beta 4)";
+                me4.Message = _helper.AddInVersion + " (beta 5)";
 #endif
                 result4 = storage.WriteMessage(me4);
             }
