c1953e1d7907aaa570de5efc9e1de88665aea0fc
Mike Williams <MikeWilliams-UK@users.noreply.github.com>
2016-03-02 14:43:43 +0000

    Beta 10

--- dev/Chem4Word.UI/UIControls/AutomaticUpdate.Designer.cs
+++ dev/Chem4Word.UI/UIControls/AutomaticUpdate.Designer.cs
@@ -34,7 +34,7 @@
             this.lblInfo = new System.Windows.Forms.Label();
             this.btnUpdateLater = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkCodeplexPage = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // btnUpdateNow
@@ -91,23 +91,23 @@
             this.progressBar1.Size = new System.Drawing.Size(447, 19);
             this.progressBar1.TabIndex = 4;
             // 
-            // linkLabel1
+            // linkCodeplexPage
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(12, 342);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(360, 13);
-            this.linkLabel1.TabIndex = 5;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Click here to download from the CodePlex web site if automatic update fails";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.linkCodeplexPage.AutoSize = true;
+            this.linkCodeplexPage.Location = new System.Drawing.Point(12, 342);
+            this.linkCodeplexPage.Name = "linkCodeplexPage";
+            this.linkCodeplexPage.Size = new System.Drawing.Size(360, 13);
+            this.linkCodeplexPage.TabIndex = 5;
+            this.linkCodeplexPage.TabStop = true;
+            this.linkCodeplexPage.Text = "Click here to download from the CodePlex web site if automatic update fails";
+            this.linkCodeplexPage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkCodeplexPage_LinkClicked);
             // 
             // AutomaticUpdate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(655, 393);
-            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.linkCodeplexPage);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btnUpdateLater);
             this.Controls.Add(this.lblInfo);
@@ -130,6 +130,6 @@
         private System.Windows.Forms.Label lblInfo;
         private System.Windows.Forms.Button btnUpdateLater;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkCodeplexPage;
     }
 }
