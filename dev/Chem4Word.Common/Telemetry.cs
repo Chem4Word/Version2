ff0f2fed8a6685b97e16aab9051303904894e344
Mike Williams <MikeWilliams-UK@users.noreply.github.com>
2016-07-14 17:13:30 +0100

    Add back passive install of Open XML SDK into installer

--- dev/Chem4Word.Common/Telemetry.cs
+++ dev/Chem4Word.Common/Telemetry.cs
@@ -87,7 +87,7 @@ namespace Chem4Word.Common
 #if DEBUG
                 me3.Message = _helper.AddInVersion + " (debug)";
 #else
-                me3.Message = _helper.AddInVersion + " RTM (06-Jul-2016)";
+                me3.Message = _helper.AddInVersion + " RTM (15-Jul-2016)";
 #endif
                 result3 = storage.WriteMessage(me3);
             }
