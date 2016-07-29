9cc505b87b80d726f8ece54482c50cb6f1bc1c1a
Mike Williams <MikeWilliams-UK@users.noreply.github.com>
2016-07-29 10:04:01 +0100

    Update R4 setup

--- dev/Chem4Word.Common/Telemetry.cs
+++ dev/Chem4Word.Common/Telemetry.cs
@@ -1,8 +1,8 @@
 ﻿// Created by Mike Williams - 22/09/2015
 // 
 // -----------------------------------------------------------------------
-//   Copyright (c) 2015, The Outercurve Foundation.  
-//   This software is released under the Apache License, Version 2.0. 
+//   Copyright (c) 2015, The Outercurve Foundation.
+//   This software is released under the Apache License, Version 2.0.
 //   The license and further copyright text can be found in the file LICENSE.TXT at
 //   the root directory of the distribution.
 // -----------------------------------------------------------------------
