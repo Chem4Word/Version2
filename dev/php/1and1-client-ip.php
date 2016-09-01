20d1a524b51f64548e700445a08db2d5d838e387
Mike Williams <MikeWilliams-UK@users.noreply.github.com>
2016-09-01 13:58:25 +0100

    Add php files

---
+++ dev/php/1and1-client-ip.php
@@ -0,0 +1,31 @@
+<?php
+// client-ip.php : Demo script by nixCraft <www.cyberciti.biz>
+// get an IP address
+$ip = $_SERVER['REMOTE_ADDR'];
+//$ip = get_client_ip_server();
+// display it back
+//echo "<h2>Client IP Demo</h2>";
+echo "Your IP address : " . $ip;
+//echo "<br>Your hostname : ". gethostbyaddr($ip) ;
+
+// Function to get the client ip address
+function get_client_ip_server() {
+    $ipaddress = '';
+    if ($_SERVER['HTTP_CLIENT_IP'])
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if($_SERVER['HTTP_X_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if($_SERVER['HTTP_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if($_SERVER['HTTP_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if($_SERVER['REMOTE_ADDR'])
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+ 
+    return $ipaddress;
+}
+?>
